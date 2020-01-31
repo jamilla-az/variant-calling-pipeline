78f86aaf54ee32d19572647651e1d02d95c17511
Jamilla Akhund-Zade <jamilla.az@gmail.com>
2020-01-31 11:30:12 -0500

    updated file path

--- poPOOLation/make_bam_lists.sh
+++ poPOOLation/make_bam_lists.sh
@@ -1,7 +1,7 @@
 #grab list of BAM files that come from a specific population - heritability
 
-ls -1 /n/debivort_lab/Jamilla_seq/subsampled_bams/*.bam | grep 'TX_[0-9]*_F' > /n/debivort_lab/Jamilla_seq/poPOOLation/bam_lists/tx_bam_herit.txt
+ls -1 /n/debivort_lab/Jamilla_seq/subsampled_bams/sorted_bams/*.bam | grep 'FL_[0-9]*_F' > /n/debivort_lab/Jamilla_seq/poPOOLation/bam_lists/fl_bam_herit.txt
 
 #grab list of BAM files that come from a specific population - variability
 
-ls -1 /n/debivort_lab/Jamilla_seq/subsampled_bams/*.bam | grep -e 'BERK_[0-9]*_[0-9]' > /n/debivort_lab/Jamilla_seq/poPOOLation/bam_lists/berk_bam_var.txt
+ls -1 /n/debivort_lab/Jamilla_seq/subsampled_bams/sorted_bams/*.bam | grep -e 'FL_[0-9]*_[0-9]\|MIA_[0-9]*_[0-9]' > /n/debivort_lab/Jamilla_seq/poPOOLation/bam_lists/fl_bam_var.txt
